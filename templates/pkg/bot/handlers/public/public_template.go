289c18c8d6fee612bb7afbb91e026a213a9c8480
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-09-28 21:29:50 +0300

    fix public

--- templates/pkg/bot/handlers/public/public_template.go
+++ templates/pkg/bot/handlers/public/public_template.go
@@ -2,15 +2,9 @@ package public
 
 import (
 	"errors"
-	"go-telebot-init
-		}
-}/pkg/database"
-	"go-telebot-init
-		}
-}/pkg/database/models"
-	"go-telebot-init
-		}
-}/pkg/utils"
+	"go-telebot-init/pkg/database"
+	"go-telebot-init/pkg/database/models"
+	"go-telebot-init/pkg/utils"
 	tele "gopkg.in/telebot.v3"
 	"gorm.io/gorm"
 	"log/slog"
